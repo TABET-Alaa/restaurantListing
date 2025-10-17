1ed41918a2e99d34c73b7c279a2367c1035d9b12
Alaaddine-ta <alae.tabet02@gmail.com>
2025-10-17 11:40:28 +0100

    added the mapping

---
+++ src/main/java/com/example/restaurantListing/mapper/RestaurantMapper.java
@@ -0,0 +1,18 @@
+package com.example.restaurantListing.mapper;
+
+
+import com.example.restaurantListing.dto.RestaurantDTO;
+import com.example.restaurantListing.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
