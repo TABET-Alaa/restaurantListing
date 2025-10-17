decdbf7129c2a15d1f59d184b520123aff384a19
Alaaddine-ta <alae.tabet02@gmail.com>
2025-10-17 11:40:48 +0100

    added the service layer

---
+++ src/main/java/com/example/restaurantListing/service/RestaurantService.java
@@ -0,0 +1,31 @@
+package com.example.restaurantListing.service;
+
+
+import com.example.restaurantListing.dto.RestaurantDTO;
+import com.example.restaurantListing.entity.Restaurant;
+import com.example.restaurantListing.mapper.RestaurantMapper;
+import com.example.restaurantListing.repo.RestaurantRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    RestaurantRepo restaurantRepo;
+
+    public List<RestaurantDTO> findAllRestaurants(){
+        List<Restaurant> restaurants = restaurantRepo.findAll();
+        List<RestaurantDTO> restaurantDTOList = restaurants.stream().map(
+                restaurant -> RestaurantMapper
+                        .INSTANCE
+                        .mapRestaurantToRestaurantDTO(restaurant))
+                        .collect(Collectors.toList()
+        );
+        return restaurantDTOList;
+    }
+}
