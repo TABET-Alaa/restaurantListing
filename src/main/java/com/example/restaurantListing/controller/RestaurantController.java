85e770b8cc15392300314c2d6f6d3021e1bcacc9
Alaaddine-ta <alae.tabet02@gmail.com>
2025-10-17 17:18:41 +0100

    added the rest

--- src/main/java/com/example/restaurantListing/controller/RestaurantController.java
+++ src/main/java/com/example/restaurantListing/controller/RestaurantController.java
@@ -6,9 +6,7 @@ import com.example.restaurantListing.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +17,35 @@ public class RestaurantController {
     @Autowired
     RestaurantService restaurantService;
 
-    @GetMapping("/restaurants")
+    @GetMapping("/fetchAllrestaurants")
     public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurant(){
         List<RestaurantDTO> allRestaurants =  restaurantService.findAllRestaurants();
         return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
     }
+
+    @PostMapping("/addRestaurant")
+    public ResponseEntity<RestaurantDTO> saveRestaurant(@RequestBody RestaurantDTO restaurantDTO){
+        RestaurantDTO restaurantAdded = restaurantService.addRestaurantInDB(restaurantDTO);
+        return new ResponseEntity<>(restaurantAdded, HttpStatus.CREATED);
+    }
+
+    @GetMapping("fetchById/{id}")
+    public ResponseEntity<RestaurantDTO> findRestaurantById(@PathVariable Integer id){
+        restaurantService.fetchRestaurantById(id);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
